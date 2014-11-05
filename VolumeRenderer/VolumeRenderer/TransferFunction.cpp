d6f9d9f96a9854193ddc6503f66d2aaa14b4ee11
Tom Noonan <noonant@tcd.ie>
2014-11-05 17:41:13 +0000

    Working on porting Joe's intensity optimization to the volume renderer.

--- VolumeRenderer/VolumeRenderer/TransferFunction.cpp
+++ VolumeRenderer/VolumeRenderer/TransferFunction.cpp
@@ -1,18 +1,38 @@
 #include "TransferFunction.h"
 
-void TransferFunction::Init(const char *filename)
+void TransferFunction::Init(const char *filename, VolumeDataset &volume_)
 {
 	colorTable.resize(256);
 	LoadXML(filename);
 	LoadLookup();
+
+	volume = &volume_;
+	CalculateFrequencies();
+	weights.resize(numIntensities);
 }
+
+void TransferFunction::CalculateFrequencies()
+{
+	frequencies.resize(256);
+	std::fill(frequencies.begin(), frequencies.end(), 0);
+
+	numVoxels = volume->xRes * volume->yRes * volume->zRes;
+
+	for (int i=0; i<numVoxels; i++)
+	{
+		float scalar = (float)volume->memblock3D[i];
+		int bin = (int)(scalar * 256.0f);
+		frequencies[bin]++;
+	}
+}
+
 
 void TransferFunction::LoadXML(const char *filename)
 {
 	tinyxml2::XMLDocument doc;
-//	auto r = doc.LoadFile("nucleon.tfi");
+	auto r = doc.LoadFile("nucleon.tfi");
 //	auto r = doc.LoadFile("../../Samples/CTknee/transfer_function/CT-Knee_spectrum_16_balance.tfi");
-	auto r = doc.LoadFile("../../Samples/downsampled vortex/90.tfi");
+//	auto r = doc.LoadFile("../../Samples/downsampled vortex/90.tfi");
 
 	if (r != tinyxml2::XML_NO_ERROR)
 		std::cout << "failed to open file" << std::endl;
@@ -79,4 +99,200 @@ glm::vec4 TransferFunction::LERPColor(glm::vec4 firstColor, glm::vec4 secondColo
 	float fraction = (currentIntensity - firstIntensity) / (secondIntensity - firstIntensity);
 
 	return firstColor + ((secondColor - firstColor) * fraction);
+}
+
+
+
+float TransferFunction::GetEntropyByID(float intensity, int index)
+{
+	float frequency = frequencies[(int)(intensity * 256)];
+		//const double epsilon = 1e-6;
+	float probability = frequency / numVoxels;
+
+	if (probability > glm::epsilon<float>())
+	{
+		float normalised = intensity / 256.0f;
+
+		return GetWeightByInterp(normalised, index) * GetOpacityByInterp(normalised, index) * probability * (-log(probability));
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+float TransferFunction::GetEntropy(int index)
+{
+	float a, b;
+
+	if (index >= 0 && index < numIntensities - 1)
+	{
+		a = intensities[index];
+		b = intensities[index + 1];
+	}
+	else
+	{
+		if (index == -1)
+		{
+			a = 0.0f;
+			b = intensities[index + 1];
+		}
+		else
+		{
+			if (index == numIntensities - 1)
+			{
+				a = intensities[index];
+				b = 1.0f;
+			}
+			else
+			{
+				std::cout << "index out of range in get_area_integral()" << std::endl;
+			}
+		}
+	}
+
+	//std::cout<<"intensity "<<a<<" "<<b;
+	a = a * 256.0f;
+	b = b * 256.0f;
+	//std::cout<<" map to [0, 255] "<<a<<" "<<b<<std::endl;
+
+	float sum = 0;
+	// int intensity belongs to [0,255]
+	for (int intensity = (int)a; intensity < b; intensity++)
+	{
+		if (intensity >= a)
+		{
+			//std::cout<<intensity<<" ";
+			sum += GetEntropyByID(intensity, index);
+		}
+	}
+	//std::cout<<std::endl;
+	return sum;
+}
+
+
+void TransferFunction::IntensityOptimize()
+{
+	int sum = 0;
+
+	for (int i=0; i<numIntensities; i++)
+	{
+		float dist = glm::abs(intensities[i] - targetIntensity);
+		weights[i] = dist;
+		sum += dist;
+	}
+
+	if (sum > 0)
+	{
+		for (int i=0; i<numIntensities; i++)
+			weights[i] /= sum;
+	}
+	
+
+	int max_index = -1;
+	int min_index = -1;
+	float max_area = std::numeric_limits<float>::min();
+	float min_area = std::numeric_limits<float>::max();
+
+
+	for (int i = 0; i<numIntensities - 1; i++)
+	{
+		if (colors[i].a > glm::epsilon<float>())
+		{
+			float area = GetEntropy(i);
+			if (area > max_area)
+			{
+				max_index = i;
+				max_area = area;
+			}
+			if (area < min_area && colors[i].a < 1.0f)
+			{
+				min_index = i;
+				min_area = area;
+			}
+		}
+	}
+}
+
+
+float TransferFunction::GetOpacityByInterp(float intensity, int index)
+{
+	int i1 = index, i2 = index + 1;
+	if (i1 >= 0 && i2 < numIntensities)
+	{
+		// linear interpolation
+		double t = (intensity - intensities[i1]) / (intensities[i2] - intensities[i1]);
+
+		double a = intensities[i1];
+		double b = intensities[i2];
+		return (a + (b - a) * t);
+	}
+	else
+	{
+		if (i1 == -1)
+		{
+			return intensities[i2];
+		}
+		else
+		{
+			if (i1 == numIntensities - 1)
+			{
+				return intensities[i1];
+			}
+			else
+			{
+				std::cout << "Errors occur in get_opacity_by_interpolation()" << std::endl;
+				return 0;
+			}
+		}
+	}
+}
+
+
+float TransferFunction::GetWeightByInterp(float intensity, int index)
+{
+	int i1 = index, i2 = index + 1;
+
+	if (i1 >= 0 && i2 < numIntensities)
+	{
+		// linear interpolation
+		double t = (intensity - intensities[i1]) / (intensities[i2] - intensities[i1]);
+
+		// get control point weights
+		double a = GetWeight(i1);
+		double b = GetWeight(i2);
+
+		return (a + (b - a) * t);
+	}
+	else
+	{
+		if (i1 == -1)
+		{
+			return GetWeight(i2);
+		}
+		else
+		{
+			if (i1 == numIntensities - 1)
+			{
+				return GetWeight(i1);
+			}
+			else
+			{
+				std::cout << "Errors occur in get_control_point_weight_by_interpolation()" << std::endl;
+				return 0;
+			}
+		}
+	}
+}
+
+float TransferFunction::GetWeight(int index)
+{
+	if (index >= 0 && index < numIntensities)
+	{
+		return weights[index];
+	}
+	else
+	{
+		return 1;
+	}
 }
--- VolumeRenderer/VolumeRenderer/TransferFunction.h
+++ VolumeRenderer/VolumeRenderer/TransferFunction.h
@@ -7,22 +7,36 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include "VolumeDataset.h"
 
 
 class TransferFunction
 {
 public:
-	std::vector<glm::vec2> divisions;
 	std::vector<glm::vec4> colors;
 	std::vector<float> intensities;
+	std::vector<int> frequencies;
+	std::vector<float> weights;
+	
 	int numIntensities;
 	GLuint tfTexture;
+	float targetIntensity;
+	int numVoxels;
+
+	VolumeDataset *volume;
 
 	std::vector<glm::vec4> colorTable;
 
-	void Init(const char *filename);
+	void Init(const char *filename, VolumeDataset &volume_);
 	void LoadXML(const char *filename);
 	void LoadLookup();
+	void IntensityOptimize();
+	float GetEntropy(int index);
+	float GetEntropyByID(float intensity, int index);
+	void CalculateFrequencies();
+	float GetOpacityByInterp(float intensity, int index);
+	float GetWeightByInterp(float intensity, int index);
+	float GetWeight(int index);
 
 	glm::vec4 LERPColor(glm::vec4 firstColor, glm::vec4 secondColor, float firstIntensity, float secondIntensity, float currentIntensity);
 };
