132332afa70fe4242ac055ce438b581df1284e24
Tom Noonan <noonant@tcd.ie>
2015-06-19 13:57:05 +0100

    Added RGBA volume renderer

---
+++ RGBAVolume/RGBAVolume/VolumeRenderer.h
@@ -0,0 +1,28 @@
+#ifndef VOLUME_RENDERER_H
+#define VOLUME_RENDERER_H
+
+#include "Camera.h"
+#include "VolumeDataset.h"
+#include "ShaderManager.h"
+#include "OpenGLRenderer.h"
+#include <time.h>
+
+class VolumeRenderer
+{
+public:
+	Camera camera;
+	GLuint shaderProgramID;
+	ShaderManager shaderManager;
+	VolumeDataset volume;
+	OpenGLRenderer *renderer;
+
+	clock_t oldTime;
+	int currentTimestep;
+
+	void Init(int screenWidth, int screenHeight);
+	void Update();
+
+};
+
+
+#endif
